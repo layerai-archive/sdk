d3f00f4db441e7fcbb5d52cc769cb81e8324b891
Cihat Imamoglu <cihat@layer.co>
2022-05-20 12:55:50 +0100

    More fixes

--- layer/pandas_extensions.py
+++ layer/pandas_extensions.py
@@ -5,7 +5,7 @@ from typing import Any, Generator, Iterable, Optional, Sequence, Type, Union
 import numpy as np
 import PIL.Image
 import pyarrow as pa
-from pandas._typing import PositionalIndexer
+from pandas._typing import PositionalIndexer  # type: ignore
 from pandas.core.arrays.base import ExtensionArray  # type: ignore
 from pandas.core.dtypes.base import register_extension_dtype  # type: ignore
 from pandas.core.dtypes.base import ExtensionDtype
