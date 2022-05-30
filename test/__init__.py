d4e15ca40ce1ab59025c9837d11a5ddea2e75607
Donatas Mažionis <donatas.mazionis@gmail.com>
2022-05-30 13:25:00 +0300

    Skip model train test for all Mac architectures

--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,15 @@
+import platform
+
+
+def _is_darwin():
+    return (platform.system() == "Darwin",)
+
+
 def _is_darwin_arm64():
-    import platform
-
-    # on M1 Mac platforms the result should be
+    # on M1 Mac platforms platform.version() returns
     # 'Darwin Kernel Version 21.5.0: Tue Apr 26 21:08:29 PDT 2022; root:xnu-8020.121.3~4/RELEASE_ARM64_T8101'
-    platform_version = platform.version()
-    return "Darwin" in platform_version and "ARM64" in platform_version
+    return _is_darwin() and ("ARM64" in platform.version())
 
 
+IS_DARWIN = _is_darwin()
 IS_DARWIN_ARM64 = _is_darwin_arm64()
