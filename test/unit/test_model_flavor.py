ca6acfddb87858213ea026d6a27ad0fc8c1f4ab5
Emin Demirci <emindemirci@gmail.com>
2022-05-30 11:19:12 +0300

    fix

--- test/unit/test_model_flavor.py
+++ test/unit/test_model_flavor.py
@@ -53,7 +53,7 @@ class TestModelFlavors:
         import xgboost as xgb
 
         model = xgb.XGBRegressor()
-        flavor = get_flavor_for_model(model, logger)
+        flavor = get_flavor_for_model(model)
         assert type(flavor).__name__ == XGBoostModelFlavor.__name__
 
     def test_sklearn_flavor(self):
