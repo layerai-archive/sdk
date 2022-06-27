141dcef19a3fc98283c3d9b1c26e7464ea34efb1
Adrien Vaschalde <adrien@layer.co>
2022-06-27 16:42:44 +0200

    Use non optional project name getter

--- layer/main.py
+++ layer/main.py
@@ -442,7 +442,7 @@ def _ensure_asset_path_is_absolute(
     project_name = (
         path.project_name
         if path.has_project()
-        else current_project_full_name().project_name
+        else get_current_project_full_name().project_name
     )
     account_name = (
         path.org_name if path.org_name is not None else current_account_name()
