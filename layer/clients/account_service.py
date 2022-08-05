80f6d0a52a8f73199acafb65cdbbfe55994ef2b4
Adrien Vaschalde <adrien@layer.co>
2022-08-05 17:44:45 +0200

    Add create organization account & delete account

--- layer/clients/account_service.py
+++ layer/clients/account_service.py
@@ -1,9 +1,12 @@
 import uuid
+from typing import Optional
 
+from layerapi.api.entity.account_view_pb2 import AccountView
 from layerapi.api.ids_pb2 import AccountId
 from layerapi.api.service.account.account_api_pb2 import (
+    CreateOrganizationAccountRequest,
+    DeleteAccountRequest,
     GetAccountViewByIdRequest,
-    GetAccountViewByIdResponse,
     GetMyAccountViewRequest,
 )
 from layerapi.api.service.account.account_api_pb2_grpc import AccountAPIStub
@@ -26,18 +29,37 @@ class AccountServiceClient:
         return client
 
     def get_account_name_by_id(self, account_id: uuid.UUID) -> str:
-        get_my_org_resp: GetAccountViewByIdResponse = (
+        account_view: AccountView = (
             self._account_api.GetAccountViewById(
                 GetAccountViewByIdRequest(id=AccountId(value=str(account_id))),
             )
-        )
-        return get_my_org_resp.account_view.name
+        ).account_view
+        return account_view.name
 
     def get_my_account(self) -> Account:
-        account_view = self._account_api.GetMyAccountView(
+        account_view: AccountView = self._account_api.GetMyAccountView(
             GetMyAccountViewRequest(),
         ).account_view
+        return self._account_from_view(account_view)
+
+    def create_organization_account(
+        self, name: str, display_name: Optional[str] = None
+    ) -> Account:
+        account_view: AccountView = self._account_api.CreateOrganizationAccount(
+            CreateOrganizationAccountRequest(
+                name=name, display_name=display_name if display_name else name
+            )
+        ).account_view
+        return self._account_from_view(account_view)
+
+    @staticmethod
+    def _account_from_view(account_view: AccountView) -> Account:
         return Account(
             id=uuid.UUID(account_view.id.value),
             name=account_view.name,
+        )
+
+    def delete_account(self, account_id: uuid.UUID) -> None:
+        self._account_api.DeleteAccount(
+            DeleteAccountRequest(account_id=AccountId(value=str(account_id)))
         )
