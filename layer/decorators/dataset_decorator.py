a976b6bd400bc38426ad3cabff432a97416c2cf6
Volkan Gurel <vol.gurel@gmail.com>
2022-08-16 23:17:24 +0300

    Use the runner function for local runs

--- layer/decorators/dataset_decorator.py
+++ layer/decorators/dataset_decorator.py
@@ -1,32 +1,11 @@
 import logging
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
-from uuid import UUID
+from typing import Any, Callable, Dict, List, Optional, Union
 
 import wrapt  # type: ignore
-from layerapi.api.ids_pb2 import ProjectId
 
 from layer import Dataset, Model
-from layer.clients.layer import LayerClient
-from layer.config import ConfigManager, is_executables_feature_active
-from layer.config.config import Config
-from layer.context import Context
-from layer.contracts.assertions import Assertion
 from layer.contracts.assets import AssetType
-from layer.contracts.datasets import DatasetBuild, DatasetBuildStatus
-from layer.contracts.definitions import FunctionDefinition
-from layer.contracts.tracker import DatasetTransferState
 from layer.decorators.layer_wrapper import LayerAssetFunctionWrapper
-from layer.global_context import reset_active_context, set_active_context
-from layer.projects.project_runner import register_function
-from layer.projects.utils import (
-    get_current_project_full_name,
-    verify_project_exists_and_retrieve_project_id,
-)
-from layer.settings import LayerSettings
-from layer.tracker.progress_tracker import RunProgressTracker
-from layer.tracker.utils import get_progress_tracker
-from layer.utils.async_utils import asyncio_run_in_thread
-from layer.utils.runtime_utils import check_and_convert_to_df
 
 
 logger = logging.getLogger(__name__)
@@ -140,133 +119,4 @@ def _dataset_wrapper(
                 dependencies,
             )
 
-        # This is not serialized with cloudpickle, so it will only be run locally.
-        # See https://layerco.slack.com/archives/C02R5B3R3GU/p1646144705414089 for detail.
-        def __call__(self, *args: Any, **kwargs: Any) -> Any:
-            if is_executables_feature_active():
-                from layer.executables.layer_runtime import local_run
-
-                return local_run(self)
-
-            self.layer.validate()
-            dataset_definition = self.get_definition()
-            dataset_definition.package()
-            config: Config = asyncio_run_in_thread(ConfigManager().refresh())
-            current_project_full_name_ = get_current_project_full_name()
-            with LayerClient(config.client, logger).init() as client:
-                progress_tracker = get_progress_tracker(
-                    url=config.url,
-                    project_name=current_project_full_name_.project_name,
-                    account_name=current_project_full_name_.account_name,
-                )
-                with progress_tracker.track() as tracker:
-                    tracker.add_asset(AssetType.DATASET, self.layer.get_asset_name())
-                    result = _build_dataset_locally_and_store_remotely(
-                        lambda: dataset_definition.func(*args, **kwargs),
-                        self.layer,
-                        dataset_definition,
-                        tracker,
-                        client,
-                    )
-                    return result
-
     return DatasetFunctionWrapper
-
-
-def _build_dataset_locally_and_store_remotely(
-    building_func: Callable[..., Any],
-    layer: LayerSettings,
-    dataset: FunctionDefinition,
-    tracker: RunProgressTracker,
-    client: LayerClient,
-) -> Any:
-    tracker.add_asset(AssetType.DATASET, layer.get_asset_name())
-
-    register_function(client, func=dataset, tracker=tracker)
-    tracker.mark_dataset_building(layer.get_asset_name())
-
-    (result, build_uuid) = _build_locally_update_remotely(
-        client,
-        building_func,
-        dataset,
-        tracker,
-    )
-
-    transfer_state = DatasetTransferState(len(result))
-    tracker.mark_dataset_saving_result(dataset.asset_name, transfer_state)
-
-    # this call would store the resulting dataset, extract the schema and complete the build from remote
-    client.data_catalog.store_dataset(
-        data=result,
-        build_id=build_uuid,
-        progress_callback=transfer_state.increment_num_transferred_rows,
-    )
-    tracker.mark_dataset_built(dataset.asset_name)
-    return result
-
-
-def _build_locally_update_remotely(
-    client: LayerClient,
-    function_that_builds_dataset: Callable[..., Any],
-    dataset: FunctionDefinition,
-    tracker: RunProgressTracker,
-) -> Tuple[Any, UUID]:
-    try:
-        with Context() as context:
-            # TODO pass path to API instead
-            current_project_uuid = verify_project_exists_and_retrieve_project_id(
-                client, dataset.project_full_name
-            )
-            initiate_build_response = client.data_catalog.initiate_build(
-                ProjectId(value=str(current_project_uuid)),
-                dataset.asset_name,
-                dataset.get_fabric(True),
-            )
-            dataset_build_id = UUID(initiate_build_response.id.value)
-            context.with_dataset_build(
-                DatasetBuild(id=dataset_build_id, status=DatasetBuildStatus.STARTED)
-            )
-            context.with_tracker(tracker)
-            context.with_asset_name(dataset.asset_name)
-            set_active_context(context)
-            try:
-                result = function_that_builds_dataset()
-                result = check_and_convert_to_df(result)
-                _run_assertions(dataset.asset_name, result, dataset.assertions, tracker)
-            except Exception as e:
-                client.data_catalog.complete_build(
-                    initiate_build_response.id, dataset.asset_name, dataset.uri, e
-                )
-                context.with_dataset_build(
-                    DatasetBuild(id=dataset_build_id, status=DatasetBuildStatus.FAILED)
-                )
-                raise e
-            reset_active_context()
-
-            context.with_dataset_build(
-                DatasetBuild(id=dataset_build_id, status=DatasetBuildStatus.COMPLETED)
-            )
-            return result, UUID(str(initiate_build_response.id.value))
-    finally:
-        reset_active_context()
-
-
-def _run_assertions(
-    asset_name: str,
-    result: Any,
-    assertions: List[Assertion],
-    tracker: RunProgressTracker,
-) -> None:
-    failed_assertions = []
-    tracker.mark_dataset_running_assertions(asset_name)
-    for assertion in reversed(assertions):
-        try:
-            tracker.mark_dataset_running_assertion(asset_name, assertion)
-            assertion.function(result)
-        except Exception:
-            failed_assertions.append(assertion)
-    if len(failed_assertions) > 0:
-        tracker.mark_dataset_failed_assertions(asset_name, failed_assertions)
-        raise Exception(f"Failed assertions {failed_assertions}\n")
-    else:
-        tracker.mark_dataset_completed_assertions(asset_name)
