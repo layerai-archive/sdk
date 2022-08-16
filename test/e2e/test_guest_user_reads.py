6efe138f5574ef5d82e6865804636dcd5f1f5e95
Cihat Imamoglu <cihat@layer.co>
2022-08-16 11:49:36 +0300

    E2E tests for reading logged data for guests

--- test/e2e/test_guest_user_reads.py
+++ test/e2e/test_guest_user_reads.py
@@ -1,12 +1,22 @@
+from uuid import UUID
+
 import pandas as pd
 import pytest
 
 import layer
 from layer import global_context
 from layer.clients.layer import LayerClient
+from layer.contracts.assets import AssetPath, AssetType
+from layer.contracts.logged_data import LoggedDataType
 from layer.contracts.projects import Project
 from layer.decorators import dataset, model
 from layer.exceptions.exceptions import LayerClientException
+
+
+dataset_log_tag = "dataset-log-tag"
+dataset_log_value = 123
+model_log_tag = "model-log-tag"
+model_log_value = 345
 
 
 @pytest.fixture()
@@ -19,6 +29,9 @@ def populated_project(initialized_project: Project) -> Project:
     def prepare_data():
         data = [["id1", 10], ["id2", 15], ["id3", 14]]
         pandas_df = pd.DataFrame(data, columns=["id", "value"])
+
+        layer.log({dataset_log_tag: dataset_log_value})
+
         return pandas_df
 
     @model("model1")
@@ -30,6 +43,9 @@ def populated_project(initialized_project: Project) -> Project:
         clf = SVC()
         result = clf.fit(iris.data, iris.target)
         print("model1 computed")
+
+        layer.log({model_log_tag: model_log_value})
+
         return result
 
     # when
@@ -53,8 +69,10 @@ def populated_public_project(
 
 
 def test_guest_user_private_dataset_read(
+    client: LayerClient,
     populated_project: Project,
     guest_context,
+    guest_client: LayerClient,
 ):
     with guest_context():
         project_path = f"{populated_project.account.name}/{populated_project.name}"
@@ -63,10 +81,23 @@ def test_guest_user_private_dataset_read(
 
         assert "not found" in str(error)
 
+        # Logged data should also not be readable.
+        dataset = client.data_catalog.get_dataset_by_name(
+            populated_project.id, "dataset1"
+        )
+
+        with pytest.raises(LayerClientException) as error:
+            guest_client.logged_data_service_client.get_logged_data(
+                tag=dataset_log_tag, dataset_build_id=dataset.build.id
+            )
+        assert "not found" in str(error)
+
 
 def test_guest_user_public_dataset_read(
+    client: LayerClient,
     populated_public_project: Project,
     guest_context,
+    guest_client: LayerClient,
 ):
     with guest_context():
         project_path = (
@@ -78,8 +109,21 @@ def test_guest_user_public_dataset_read(
         assert df.values[0][0] == "id1"
         assert df.values[0][1] == 10
 
+        dataset = client.data_catalog.get_dataset_by_name(
+            populated_public_project.id, "dataset1"
+        )
+
+        logged_data = guest_client.logged_data_service_client.get_logged_data(
+            tag=dataset_log_tag, dataset_build_id=dataset.build.id
+        )
+
+        assert logged_data.data == str(dataset_log_value)
+        assert logged_data.logged_data_type == LoggedDataType.NUMBER
+        assert logged_data.tag == dataset_log_tag
+
 
 def test_guest_user_private_model_read(
+    client: LayerClient,
     populated_project: Project,
     guest_context,
 ):
@@ -91,19 +135,49 @@ def test_guest_user_private_model_read(
         assert "not found" in str(error)
 
 
+def test_guest_user_private_model_logged_data_read(
+    client: LayerClient,
+    populated_project: Project,
+    guest_context,
+    guest_client: LayerClient,
+):
+    project_path = f"{populated_project.account.name}/{populated_project.name}"
+    name = f"{project_path}/models/model1"
+    asset_path = AssetPath.parse(name, expected_asset_type=AssetType.MODEL)
+    mdl = client.model_catalog.load_model_by_path(path=asset_path.path())
+
+    with guest_context():
+        with pytest.raises(LayerClientException) as error:
+            guest_client.logged_data_service_client.get_logged_data(
+                tag=model_log_tag, train_id=UUID(mdl.storage_config.train_id.value)
+            )
+        assert "not found" in str(error)
+
+
 def test_guest_user_public_model_read(
+    client: LayerClient,
     populated_public_project: Project,
     guest_context,
+    guest_client: LayerClient,
 ):
     with guest_context():
         project_path = (
             f"{populated_public_project.account.name}/{populated_public_project.name}"
         )
-        train = layer.get_model(f"{project_path}/models/model1").get_train()
+        mdl = layer.get_model(f"{project_path}/models/model1")
+        train = mdl.get_train()
 
         from sklearn.svm import SVC
 
         assert isinstance(train, SVC)
+
+        logged_data = client.logged_data_service_client.get_logged_data(
+            tag=model_log_tag, train_id=UUID(mdl.storage_config.train_id.value)
+        )
+
+        assert logged_data.data == str(model_log_value)
+        assert logged_data.logged_data_type == LoggedDataType.NUMBER
+        assert logged_data.tag == model_log_tag
 
 
 def test_guest_user_cannot_init_project(
