bd18c75661d1622d74c6345d4def3ae865d7f562
Emin Demirci <emindemirci@gmail.com>
2022-08-17 11:11:59 +0300

    cached_property is not supported on 3.7

--- layer/projects/execution_planner.py
+++ layer/projects/execution_planner.py
@@ -1,8 +1,7 @@
 import sys
 from collections import defaultdict, deque
 from dataclasses import dataclass
-from functools import cached_property
-from typing import TYPE_CHECKING, DefaultDict, List, Sequence, Tuple
+from typing import TYPE_CHECKING, DefaultDict, List, Sequence
 
 from layerapi.api.entity.operations_pb2 import (
     ExecutionPlan,
@@ -34,10 +33,11 @@ class PlanNode:
     fabric: Fabric
     package_download_url: str
     dependencies: List[AssetPath]
-
-    @cached_property
-    def _language_version() -> Tuple[int, int, int]:
-        return sys.version_info.major, sys.version_info.minor, sys.version_info.micro
+    language_version: "LanguageVersion" = LanguageVersion(
+        major=sys.version_info.major,
+        minor=sys.version_info.minor,
+        micro=sys.version_info.micro,
+    )
 
     def to_execution_operation(self) -> FunctionExecutionOperation:
         task_type = Task.Type.TYPE_INVALID
@@ -46,7 +46,6 @@ class PlanNode:
         elif self.path.asset_type == AssetType.MODEL:
             task_type = Task.Type.TYPE_MODEL_TRAIN
         dependencies = [d.path() for d in self.dependencies]
-        language_version = self._language_version()
 
         return FunctionExecutionOperation(
             task_type=task_type,
@@ -54,11 +53,7 @@ class PlanNode:
             executable_package_url=self.package_download_url,
             fabric=self.fabric.value,
             dependency=dependencies,
-            language_version=LanguageVersion(
-                major=language_version[0],
-                minor=language_version[1],
-                micro=language_version[2],
-            ),
+            language_version=self.language_version,
         )
 
 
