79d734730e6045ae597a3433d15a3b3362b1a13b
Aleks Mitov <1766321+aleksmitov@users.noreply.github.com>
2022-08-25 14:39:18 +0300

    Fail fast when running on Windows without WSL (#362)
    Stop execution when trying to run the SDK on Windows without using WSL.

--- layer/main/utils.py
+++ layer/main/utils.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Any, Callable
 
 from .version import check_latest_version
@@ -5,7 +6,21 @@ from .version import check_latest_version
 
 def sdk_function(func: Callable[..., Any]) -> Callable[..., Any]:
     def inner(*args: Any, **kwargs: Any) -> Any:
+        _check_os()
         check_latest_version()
         return func(*args, **kwargs)
 
     return inner
+
+
+def _check_os() -> None:
+    import platform
+
+    def is_windows_os() -> bool:
+        return platform.system() == "Windows"
+
+    if is_windows_os():
+        print(
+            "Windows is not supported. Please, have a look at https://docs.app.layer.ai/docs/installation"
+        )
+        sys.exit()
