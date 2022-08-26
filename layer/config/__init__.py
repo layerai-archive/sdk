66d03daa501666108f11ff805be8df6e3963e348
maasg <gerard.maas@gmail.com>
2022-08-26 18:03:17 +0200

    Remove 'EXECUTABLE_FEATURE' flag

--- layer/config/__init__.py
+++ layer/config/__init__.py
@@ -14,18 +14,3 @@ from .config import (  # noqa
 )
 from .config_client import ConfigClient  # noqa
 from .config_manager import ConfigManager  # noqa
-
-
-def is_executables_feature_active() -> bool:
-    import os
-
-    if "LAYER_EXECUTABLES" in os.environ:
-        return True
-
-    try:
-        config = ConfigManager().load()
-    except:  # noqa
-        return False
-
-    # enable by default for non production environments
-    return config.url != DEFAULT_URL
--- layer/main/run.py
+++ layer/main/run.py
@@ -2,7 +2,7 @@ import logging
 from typing import Any, Callable, List, Optional
 
 from layer.clients.layer import LayerClient
-from layer.config import ConfigManager, is_executables_feature_active
+from layer.config import ConfigManager
 from layer.config.config import Config
 from layer.contracts.fabrics import Fabric
 from layer.contracts.project_full_name import ProjectFullName
@@ -134,20 +134,13 @@ def run(
             ray_address=ray_address,
         )
         run = ray_project_runner.run()
-    elif kwargs.get("executables_feature", False) or is_executables_feature_active():
+    else:
         project_runner = ProjectRunner(
             config=layer_config,
             project_full_name=project_full_name,
             functions=functions,
         )
         run = project_runner.run(debug=debug)
-    else:
-        old_project_runner = OldProjectRunner(
-            config=layer_config,
-            project_full_name=project_full_name,
-            functions=functions,
-        )
-        run = old_project_runner.run(debug=debug)
     _make_notebook_links_open_in_new_tab()
     return run
 
