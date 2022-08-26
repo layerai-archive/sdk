4e98d55aa1a90f6df237f8186546941dbc9285ba
Volkan Gurel <vol.gurel@gmail.com>
2022-08-26 09:45:49 -0400

    Ignore setuptools warnings

--- layer/main/__init__.py
+++ layer/main/__init__.py
@@ -6,3 +6,9 @@ warnings.filterwarnings(
     "`should_run_async` will not call `transform_cell` automatically in the future.",
     DeprecationWarning,
 )
+
+warnings.filterwarnings(
+    "ignore",
+    "Setuptools is replacing distutils.",
+    UserWarning,
+)
