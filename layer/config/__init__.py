b04283273da9e4853a25c00a0a398f4f6e93412b
Donatas Mažionis <donatas.mazionis@gmail.com>
2022-08-31 13:44:27 +0300

    Remove executables flag

--- layer/config/__init__.py
+++ layer/config/__init__.py
@@ -14,18 +14,3 @@ from .config import (  # noqa
 )
 from .config_client import ConfigClient  # noqa
 from .config_manager import ConfigManager  # noqa
-
-
-def is_executables_feature_active() -> bool:
-    import os
-
-    if "LAYER_EXECUTABLES" in os.environ and not _str2bool(
-        os.environ["LAYER_EXECUTABLES"]
-    ):
-        return False
-    # enabled by default
-    return True
-
-
-def _str2bool(v: str) -> bool:
-    return v.lower() not in ("no", "false", "0")
