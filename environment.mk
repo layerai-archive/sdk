d8c53eb7f33387859cfb795745c470e75a63384c
Dimitar Popov <dimitar@layer.co>
2022-09-05 17:41:58 +0100

    Fix m1 conda env

--- environment.mk
+++ environment.mk
@@ -75,6 +75,8 @@ else
                          $(call get_python_package_version,h5py) \
                          $(call get_python_package_version,pyarrow) \
                          $(call get_python_package_version,grpcio)
+## https://github.com/python-poetry/poetry/issues/6408
+	@find build/$(PROJECT_NAME)/lib -name direct_url.json -delete
 endif
 else
 	@echo 'Not inside a conda environment or conda not installed, this is a requirement for Apple arm processors'
